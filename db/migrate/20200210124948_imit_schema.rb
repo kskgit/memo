e3a7ddda27fc72d020d84e8da264b47a13c3e25a
keisuke <keisuke@diffeasy.com>
2020-02-10 23:52:46 +0900

    create db

--- db/migrate/20200210124948_imit_schema.rb
+++ db/migrate/20200210124948_imit_schema.rb
@@ -7,6 +7,32 @@ class ImitSchema < ActiveRecord::Migration[5.2]
       t.string "pass_word", null: false
       t.datetime "discarded_at"
     end
+    create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+      t.string "project_name", null: false
+      t.datetime "discarded_at"
+    end
+    create_table "project_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+      t.integer "project_id", null: false
+      t.integer "user_id", null: false
+      t.datetime "discarded_at"
+    end
+    create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+      t.integer "project_id", null: false
+      t.integer "post_user_id", null: false
+      t.string "memo", null: false
+      t.datetime "post_date", null: false
+      t.datetime "discarded_at"
+    end
+    create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+      t.integer "memo_id", null: false
+      t.integer "tagu", null: false
+      t.datetime "discarded_at"
+    end
+    create_table "attendees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+      t.integer "memo_id", null: false
+      t.integer "user_id", null: false
+      t.datetime "discarded_at"
+    end
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_02_10_124948) do
+
+  create_table "attendees", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "memo_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "discarded_at"
+  end
+
+  create_table "memos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "post_user_id", null: false
+    t.string "memo", null: false
+    t.datetime "post_date", null: false
+    t.datetime "discarded_at"
+  end
+
+  create_table "project_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "project_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "discarded_at"
+  end
+
+  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "project_name", null: false
+    t.datetime "discarded_at"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.integer "memo_id", null: false
+    t.integer "tagu", null: false
+    t.datetime "discarded_at"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "mail", null: false
+    t.string "pass_word", null: false
+    t.datetime "discarded_at"
+  end
 
 end
