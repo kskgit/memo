0d504cec31c78a11bc1f8605fcf5824903081d53
keisuke <keisuke@diffeasy.com>
2020-03-08 23:17:45 +0900

    画像保存・削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :users do
+    member do
+      delete :delete_image
+    end
+  end
+
   resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
