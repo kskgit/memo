3be63716cdcda9de22a93ccae421466697f18d88
keisuke <keisuke@diffeasy.com>
2020-08-16 22:06:59 +0900

    テスト追加

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 RSpec.describe BooksController, type: :controller do
   describe "#index" do
-    # 正常なレスポンスか？
     it "responds successfully" do
       get :index, params: {is_readed: false, uid: 'glzm8HISc6SK1qk6L20HSQZrcZN2'}
       expect(response.status).to eq(200)
@@ -12,7 +11,6 @@ end
 
 RSpec.describe BooksController, type: :controller do
   describe "#create" do
-    # 正常なレスポンスか？
     it "responds successfully" do
       post :create, params: {
         author: 'タイトル',
@@ -20,6 +18,37 @@ RSpec.describe BooksController, type: :controller do
         uid: 'glzm8HISc6SK1qk6L20HSQZrcZN2'
       }
       expect(response.status).to eq(201)
+    end
+  end
+end
+
+RSpec.describe BooksController, type: :controller do
+  describe "#create_by_multipart_form" do
+    it "responds successfully" do
+      post :create_by_multipart_form, params: {
+        author: 'タイトル',
+        title: '著者',
+        uid: 'glzm8HISc6SK1qk6L20HSQZrcZN2'
+      }
+      expect(response.status).to eq(201)
+    end
+  end
+end
+
+RSpec.describe BooksController, type: :controller do
+  describe "#update" do
+    it "responds successfully" do
+      get :update, params: {id: 23, page_number: 10}
+      expect(response.status).to eq(200)
+    end
+  end
+end
+
+RSpec.describe BooksController, type: :controller do
+  describe "#destroy" do
+    it "responds successfully" do
+      get :destroy, params: {id: 23}
+      expect(response.status).to eq(204)
     end
   end
 end
